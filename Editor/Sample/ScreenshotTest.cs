a1e7bf8c03d25d43285ff73d09a1a42d34a77cdc
Thomas Hummes <info@tastenhacker.de>
2016-03-13 13:09:24 +0100

    Updated Readme and SampleFile

--- Editor/Sample/ScreenshotTest.cs
+++ Editor/Sample/ScreenshotTest.cs
@@ -6,20 +6,19 @@ namespace UnityEditorHelper
     [CustomEditor(typeof (SampleScript))]
     public class ScreenshotTest : Editor
     {
+        public Vector2 _scrollPos;
+        private bool state;
+
         public override void OnInspectorGUI()
         {
-            using (new HighlightBox())
+            using (new FoldableBlock(ref state, "Foldable Block"))
             {
-                EditorGUILayout.Slider("Range property", 5, 0, 10);
-                EditorGUILayout.TextField("Sample Field",  GUILayout.Height(150));
-                EditorGUILayout.ObjectField("Object Field", null, typeof (Transform), true);
-            }
-
-            using (new HighlightBox(Color.red))
-            {
-                EditorGUILayout.Slider("Range property", 5, 0, 10);
-                EditorGUILayout.TextField("Sample Field", GUILayout.Height(150));
-                EditorGUILayout.ObjectField("Object Field", null, typeof(Transform), true);
+                if(state)
+                {
+                    EditorGUILayout.Slider("Range property", 5, 0, 10);
+                    EditorGUILayout.TextField("Sample Field", GUILayout.Height(150));
+                    EditorGUILayout.ObjectField("Object Field", null, typeof (Transform), true);
+                }
             }
         }
     }
